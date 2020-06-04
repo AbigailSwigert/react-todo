f3306d6d92ef86490a03c7ae6ebbc6895e0d5d61
Abigail Swigert <abigail@swigertdesigns.com>
2020-06-04 17:33:58 -0500

    added toggle checkbox when task is clicked

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -4,15 +4,20 @@ import React, {useState} from 'react';
 export default function Tasks({input, handleDelete, id}) {
 
     const [fadeOut, setFadeOut] = useState(false);
+    const [completed, setCompleted] = useState(false);
 
     const addFadeOut = () => {
         setFadeOut(true);
     }
 
+    const toggleChecked = () => {
+        setCompleted(!completed);
+    }
+
     return (
-        <div className={fadeOut ? 'task fade-out' : 'task'}>
+        <div className={fadeOut ? 'task fade-out' : 'task'} onClick={toggleChecked} >
             <p className='task-text' >
-                <input type='checkbox' className='task-completed' />
+                <input type='checkbox' className='task-completed' onChange={toggleChecked} checked={completed} />
                 {input}
             </p>
             <button className='delete-btn' onClick={() => {handleDelete(id); addFadeOut();}} >Delete</button>
